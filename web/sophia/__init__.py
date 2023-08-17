a11c46feb69e91e2ece3801382472a82bb92f1c2
applepiea <saturn2017@naver.com>
2023-08-17 16:03:07 +0900

    feat: web init

---
+++ web/sophia/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import config
+
+# sql session 들어가야함
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(config)
+
+    # from . import models
+    from .views import main_views, user_views, librarian_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(user_views.bp)
+    app.register_blueprint(librarian_views.bp)
+
+
+    return app
