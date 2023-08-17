ccdde97bff248a42afd8fd944602c96260e1c25b
jeanDeluge <jeandelugedev@gmail.com>
2023-08-18 04:53:35 +0900

    feat: 기능명서

---
+++ utils/find_books/find_reversed_books.py
@@ -0,0 +1,15 @@
+# 기능 개요
+# 책장을 찍으면 뒤집어진 책을 찾아 표시해줌
+# 상황 가정
+# 1. 장서 점검시
+# input
+# 책 사진 위치
+# output
+# 책 사진 위에  그려진 reversebook만 표기된 사진을 저장한 위치
+import pandas as pd
+
+def find_reversed_books(bookshelf_photo_location: str)-> str:
+    pass
+    
+if __name__ == "__main__":
+    print(find_reversed_books("사이먼 몽크 지음 ;윤진서 옮김"))
