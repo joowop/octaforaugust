404d28f77fe49a1f66963e97e448a5102b576bbe
jeanDeluge <jeandelugedev@gmail.com>
2023-08-19 23:22:02 +0900

    chore: reversed 위치

--- web/sophia/views/librarian_views.py
+++ web/sophia/views/librarian_views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, send_file
 from werkzeug.utils import redirect
+from ..apis.librarian import Libarian
 # from sophia.models import
 # from sophia.forms import
 
@@ -13,11 +14,9 @@ def missing():
     return "이러한 책들이 분실되었습니다."
 
 # 뒤집어진 책 찾기
-@bp.route('/reversed_book', methods=['GET','POST']) # 받아오는게 없을 때는 post 단독으로 열 수 없음
+@bp.route('/reversed_book', methods=['POST'])
 def reversed():
-    # 모델 연결
-    print(request.files)
-    return "이 책들은 잘못 꽂혔습니다."
+     # Libarian(request=request).reversed_book_location()  여기서 이미지 주소 불러올 수 있습니다~~~!
 
 # 잘못 꽂힌 책 찾기
 @bp.route('/wrong_placed_book', methods=['GET','POST'])
