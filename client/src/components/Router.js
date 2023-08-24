f9616b0aac1071724574d73c556177b23801ce29
jeanDeluge <jeandelugedev@gmail.com>
2023-08-24 12:22:46 +0900

    bug: route error

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -3,19 +3,24 @@ import { Route, Routes } from 'react-router-dom'
 import { ROUTE_PATH_LIST } from '../Constant'
 
 function RouteList(){
-    return (
+    return(
+        <>
         <Routes>
-            <Route path={ROUTE_PATH_LIST.Main.path} element={<ROUTE_PATH_LIST.Main.component />} />
-            <Route path={ROUTE_PATH_LIST.LibrarianMain.path} element={<ROUTE_PATH_LIST.LibrarianMain.component />}>
-                <Route path={ROUTE_PATH_LIST.LibrarianCheckCollection.path} element={<ROUTE_PATH_LIST.LibrarianCheckCollection.component />} />
-                <Route path={ROUTE_PATH_LIST.LibrarianOrganization.path} element={<ROUTE_PATH_LIST.LibrarianOrganization.component />} />
+            <Route path={ROUTE_PATH_LIST.Main.path} element={ROUTE_PATH_LIST.Main.component}/>
+            <Route path={ROUTE_PATH_LIST.LibrarianMain.path} element={ROUTE_PATH_LIST.LibrarianMain.element}>
+                <Route path={ROUTE_PATH_LIST.LibrarianCheckCollection.path} element={ROUTE_PATH_LIST.LibrarianCheckCollection.component}/>
+                <Route path={ROUTE_PATH_LIST.LibrarianOrganization.path} element={ROUTE_PATH_LIST.LibrarianOrganization.component}/>
+
             </Route>
-            <Route path={ROUTE_PATH_LIST.LibraryUserMain.path} element={<ROUTE_PATH_LIST.LibraryUserMain.component />}>
-                <Route path={ROUTE_PATH_LIST.FindBook.path} element={<ROUTE_PATH_LIST.FindBook.component />} />
-                <Route path={ROUTE_PATH_LIST.RecommendBook.path} element={<ROUTE_PATH_LIST.RecommendBook.component />} />
+            <Route path={ROUTE_PATH_LIST.LibraryUserMain.path} element={ROUTE_PATH_LIST.LibraryUserMain.element}>
+                <Route path={ROUTE_PATH_LIST.FindBook.path} element={ROUTE_PATH_LIST.FindBook.component}/>
+                <Route path={ROUTE_PATH_LIST.RecommendBook.path} element={ROUTE_PATH_LIST.RecommendBook.component}/>
+
             </Route>
         </Routes>
+        </>
     );
 }
+
 
 export default RouteList;
