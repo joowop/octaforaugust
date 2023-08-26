95601d020197e19567b9f7c04cbc7ea9d27d39ff
jeanDeluge <jeandelugedev@gmail.com>
2023-08-26 19:28:14 +0900

    chore: 디렉토리 정리

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,25 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import Navi from './navi'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { Navigation } from "./components";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Footer } from "flowbite-react";
 
-function Home() {
-  return (
-    <div className="Body">
-      얍
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-       <Navi />
-      <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-
+function App(){
+  return(
+    <>
+    <div className="max-w-auto">
+      <header>
+        <Router>
+          <Navigation />
+        </Router>
       </header>
-         <Routes>
-          <Route path="/" element={<Home />} /> {/* 경로가 / 인 경우 Home 컴포넌트 렌더링 */}
-          {/* 더 많은 라우트 설정 */}
-        </Routes>
-        </BrowserRouter>
- </div>
+      <main className="h-auto">
+
+      </main>
+        <footer>
+          <Footer container />
+      </footer>
+    </div>
+    </>
   );
 }
 
