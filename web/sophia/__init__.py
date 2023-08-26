da26c30a3cd90b95c830d2ced3754967d4c1dc5d
jeanDeluge <jeandelugedev@gmail.com>
2023-08-26 19:25:41 +0900

    feat: cors

--- web/sophia/__init__.py
+++ web/sophia/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 # import config
 
 # sql session 들어가야함
@@ -13,5 +14,5 @@ def create_app():
     app.register_blueprint(user_views.bp)
     app.register_blueprint(librarian_views.bp)
 
-
+    CORS(app, resources={r'*': {'origins': 'http://127.0.0.1:3000'}}, supports_credentials=True)
     return app
