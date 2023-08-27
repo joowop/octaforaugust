8b9e1b4b38bd62b83423dae39a13f26bfa363821
jeanDeluge <jeandelugedev@gmail.com>
2023-08-27 11:53:41 +0900

    feat: 카메라 적용

--- client/src/pages/Librarian/Sidemenu/OrgarnizeCollections/OrganizingCollections.js
+++ client/src/pages/Librarian/Sidemenu/OrgarnizeCollections/OrganizingCollections.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { ROUTE_PATH_LIST } from '../../../../Constant';
 import { Navbar } from 'flowbite-react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 function OrganizingCollections() {
   return (
     <>
       <Navbar>
-        <Navbar.Link href={ROUTE_PATH_LIST.OrganizeAll.path}>모든 장서 정리</Navbar.Link>
-        <Navbar.Link href={ROUTE_PATH_LIST.OrganizeOne.path}>책장 정리</Navbar.Link>
-        <Navbar.Link href={ROUTE_PATH_LIST.OrganizeNow.path}>책장 정리 Beta</Navbar.Link>
+        <Link to={ROUTE_PATH_LIST.OrganizeAll.path}>모든 장서 정리</Link>
+        <Link to={ROUTE_PATH_LIST.OrganizeOne.path}>책장 정리</Link>
+        <Link to={ROUTE_PATH_LIST.OrganizeNow.path}>책장 정리 Beta</Link>
       </Navbar>
+
       <Outlet/>
     </>
   );
