96a99840db49ad66a5bab167465561ec07809f28
jeanDeluge <jeandelugedev@gmail.com>
2023-08-28 07:47:17 +0900

    feat: 실시간 확인 버튼

--- client/src/pages/Librarian/Sidemenu/LiveOrganizingCollections.js
+++ client/src/pages/Librarian/Sidemenu/LiveOrganizingCollections.js
@@ -1,12 +1,24 @@
 /* eslint-disable no-undef */
-import React from 'react';
-
+import React, { useState } from 'react';
+import { Button } from 'flowbite-react';
 function LiveOrganization() {
+
+
+  const [click, setClick] = useState(false)
+
+  const handelClick =  ()=> {
+    setClick(!click)
+  }
+
 
   return (
     <>
-   <video muted autoPlay controls src='https://cdn.jsdelivr.net/gh/jeanDeluge/CDN/1.mp4'>
-   </video>
+
+    <div className='flex'>
+    <Button onClick={handelClick}>실시간 확인</Button>
+    {click && (<video muted controls src='https://cdn.jsdelivr.net/gh/jeanDeluge/CDN/1.mp4'>
+    </video>)}
+    </div>
    </>
   );
 }
